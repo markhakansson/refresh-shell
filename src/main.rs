97a405e2eedd6d6d2045ad383c26cfa69ff82e8e
Mark Hakansson <mark.hakansson@live.se>
2019-03-18 08:38:24 +0100

    Fix a few bugs.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::process;
 use std::process::Command;
 use std::path::Path;
 use std::env;
-use std::str::SplitWhitespace;
 
 fn main() {
     loop {
@@ -16,38 +15,54 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
        
         // Split input
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
-        
-        read_command(command, args);
-
+        let mut parts = input.trim().split_whitespace().peekable();
+        if parts.peek().is_some() {
+            let command = parts.next().unwrap();
+            let args = parts;
+            read_command(command, args);
+        } 
     }
 }
 
 // Checks the input and runs the given command
-fn read_command(command: &str, args: SplitWhitespace) {
+fn read_command<'a,I>(command: &str, args: I) 
+    where I: Iterator<Item = &'a str>,
+{ 
     match command {
         "cd" => {
-            let direction = args.peekable().peek().map_or("/", |x| *x);
-            let root = Path::new(direction);
-            if let Err(e) = env::set_current_dir(&root) {
-                eprintln!("{}", e);
-            }
+            cd(args);
         },
 
-        "exit" => process::exit(1),
+        "exit" => process::exit(0),
 
         command => {
-            // Run command
-            let mut child = Command::new(command)
-                .args(args)
-                .spawn();
-
-            match child {
-                Ok(mut child) => { child.wait(); },
-                Err(e) => eprintln!("{}", e),
-            }
+            run_command(command, args);
         }
+    }
+}
+
+// Runs external command
+fn run_command<'a,I>(command: &str, args: I) 
+    where I: Iterator<Item = &'a str>,
+{
+    // Run command
+    let child = Command::new(command)
+        .args(args)
+        .spawn();
+
+    match child {
+        Ok(mut child) => { child.wait(); },
+        Err(e) => eprintln!("{}", e),
+    }
+}
+
+// Shell builtin cd
+fn cd<'a,I>(args: I) 
+    where I: Iterator<Item=&'a str>, 
+{    
+    let direction = args.peekable().peek().map_or("/", |x| *x);
+    let root = Path::new(direction);
+    if let Err(e) = env::set_current_dir(&root) {
+        eprintln!("{}", e);
     }
 }
